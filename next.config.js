00d92ad8363c3f51d7a15864be1d3b8689c19eb8
Kiranism <iamkiran2831@gmail.com>
2023-09-14 02:55:28 +0530

    deploying errors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  webpack: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+  },
   experimental: {
     serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
   },
