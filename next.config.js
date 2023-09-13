7efd6f8c923f517d897998a59943b085d66c35ef
Kiranism <iamkiran2831@gmail.com>
2023-09-14 02:59:08 +0530

    deploying errors

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: "standalone",
   webpack: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+    config.resolve.fallback = { fs: false, os: false };
     return config;
   },
   experimental: {
