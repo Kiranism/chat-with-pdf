f39b757fa9dc2b70007311715c992ca69aaffe87
Kiranism <iamkiran2831@gmail.com>
2023-09-14 02:57:33 +0530

    deploying errors config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   webpack: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
+    return config;
   },
   experimental: {
     serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
