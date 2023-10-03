3884de5f3ab2f4cf734f9fdddcb755f65317f567
Kiranism <iamkiran2831@gmail.com>
2023-10-04 00:37:39 +0530

    write file path

--- src/lib/downloadFile.ts
+++ src/lib/downloadFile.ts
@@ -17,7 +17,9 @@ export async function downloadFromURL(file_url: string) {
     // const tempDirectory = process.env.TEMP! || process.env.TMP!;
     const tempDirectory = os.tmpdir();
 
-    const file_name = path.join(process.cwd(), `pdf-${Date.now()}.pdf`);
+    // const file_name = path.join(process.cwd(), `pdf-${Date.now()}.pdf`);
+    const file_name = path.join("/tmp", `pdf-${Date.now()}.pdf`);
+
     // const file_name = `/temp/pdf-${Date.now()}.pdf`;
     // fs.writeFileSync(file_name, obj.Body as Buffer);
     fs.writeFileSync(file_name, pdfContent);
